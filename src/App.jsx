c73859d3f35c955673254e6d9123bcdd962f109a
Vengateshwaran1 <kvengateshwaran1@gmail.com>
2024-08-10 20:28:04 +0530

    committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
-import Skills from "./components/Skills";
+import Skills from './components/Skills'
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
